0735618d7e73dd078b98d1f9e7e703f2ab7837dc
Priyanka <tayi.priyanka@gmail.com>
2026-07-02 15:08:15 -0400

    correccted URL

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,7 @@ function App() {
 
     try {
       const response = await axios.post(
-        "https://smarttalk-ai.vercel.app/api/chat",
+        "http://localhost:5000/api/chat",
         {
           prompt: [prompt],
         },
